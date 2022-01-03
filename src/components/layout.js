8c3104e6a18998bc47d6beecd1af4d42e9ad2d27
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-01-04 00:12:53 +0600

    build v

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,7 @@ import Header from "./header"
 import "./layout.css"
 import LoopFunction from "./typescript/antd/LoopBanner"
 import CardFunct from "./typescript/antd/CardFunc"
+import resTablefun from "./typescript/csp/Table"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -39,6 +40,7 @@ const Layout = ({ children }) => {
           {children}
           <LoopFunction />
           <CardFunct />
+          <resTablefun />
         </main>
 
         <footer
